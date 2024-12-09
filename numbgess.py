df39a9075da96a97f66087e8fb866275c7770988
Pin3appl3ishan <sthaishan2004@gmail.com>
2024-12-09 18:59:30 +0545

    first commit

---
+++ numbgess.py
@@ -0,0 +1,26 @@
+# program to guess a number randomly 
+
+from random import randint
+
+lower_num, higher_num = 1, 10
+random_number: int = randint(lower_num,higher_num)
+print(f'Guess the number in the range {lower_num} to {higher_num}')
+
+while True:
+    try:
+        user_guess: int = int(input('Guess: '))
+    except ValueError as e:
+        print('Please enter a valid number :')
+        continue
+
+    if user_guess > random_number:
+        print('The number is lower')
+    elif user_guess < random_number:
+        print('The number is higher')
+    else:
+        print('You guessed it')
+        break
+
+
+
+# add limit to number a times a user can guess 
